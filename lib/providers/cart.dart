533dabd3159c45c125b38ac80cc2f5cc75d91739
Eduardo Reche Lopes <earlope@xomdeveloper.com>
2021-02-02 22:43:17 -0300

    cart total and order now button

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -25,6 +25,15 @@ class Cart with ChangeNotifier {
     return _items.length;
   }
 
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((_, value) {
+      total += value.price;
+    });
+
+    return total;
+  }
+
   void addItem(String productId, double price, String title) {
     if (_items.containsKey(productId)) {
       _items.update(
---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
+
+class CartScreen extends StatelessWidget {
+  static const routeName = '/cart';
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your cart'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(color: theme.primaryColor),
+                    ),
+                    backgroundColor: theme.accentColor,
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'ORDER NOW',
+                      style: TextStyle(color: theme.primaryColor),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
